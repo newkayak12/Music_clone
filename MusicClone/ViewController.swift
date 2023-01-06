529786832543c7f7ea4835fa87a5ba7796c5711e
newkayak12 <newkayak12@gmail.com>
2023-01-06 22:07:16 +0900

    init

---
+++ MusicClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicClone
+//
+//  Created by Sang Hyeon kim on 2023/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
